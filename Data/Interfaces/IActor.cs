edbd507a068326261adda9ae37e93dff87cb169a
GettReady <eroxinand@gmail.com>
2023-03-12 22:24:31 +0300

    Added IActor interface

---
+++ Data/Interfaces/IActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wyrm.Data
+{
+    interface IActor
+    {
+        public bool IsPaused { get; }
+
+        public Task Act();
+        public Task Spawn();
+        public void Pause();
+        public void Unpause();
+    }
+}
